c866d5f67e0fbc4aee3d95d2a0cc4def63903fc0
Fanny <venezia.fanny@gmail.com>
2023-01-09 11:20:42 +0100

    Création navbar et ajout vidéo sur la homepage

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,6 @@
 class CoursesController < ApplicationController
   before_action :set_course, only: %i[show edit update destroy]
+  skip_before_action :authenticate_user!, only: %i[index show]
 
   def index
     @courses = Course.all
